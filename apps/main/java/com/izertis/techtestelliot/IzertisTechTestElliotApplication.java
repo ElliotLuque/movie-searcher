1b50b3481c2b39b8a60477253c4936c3722bb404
ElliotLuque <elliotluque@gmail.com>
2025-06-18 11:36:07 +0200

    initial commit

---
+++ apps/main/java/com/izertis/techtestelliot/IzertisTechTestElliotApplication.java
@@ -0,0 +1,13 @@
+package com.izertis.techtestelliot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IzertisTechTestElliotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IzertisTechTestElliotApplication.class, args);
+    }
+
+}
