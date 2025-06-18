f04a551c0d6bf2cade82cb89f0285d782c39636d
Elliot <elliotluque@gmail.com>
2025-06-18 20:00:33 +0200

    feat: starter configuration processor, properties class

---
+++ src/backend/main/java/com/izertis/techtestelliot/config/MoviesProperties.java
@@ -0,0 +1,28 @@
+package com.izertis.techtestelliot.config;
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Data
+@Component
+@ConfigurationProperties(prefix = "movies")
+public class MoviesProperties {
+
+    private String provider = "tmdb";
+
+    private final Tmdb tmdb = new Tmdb();
+    private final Omdb omdb = new Omdb();
+
+    @Data
+    public static class Tmdb {
+        private String baseUrl;
+        private String apiKey;
+        private String language = "en-US";
+    }
+
+    @Data
+    public static class Omdb {
+        private String baseUrl;
+        private String apiKey;
+    }
+}
