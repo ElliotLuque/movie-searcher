d792365bdd71aed34a8b533c6677c1a3e789139d
Elliot <elliotluque@gmail.com>
2025-06-22 13:56:56 +0200

    chore: initial angular config

--- src/frontend/src/app/app.routes.ts
+++ src/frontend/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import {MovieListPage} from './modules/movies/list/movie-list-page.component';
+import {MovieDetailsPage} from './modules/movies/pages/details/movie-details-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: MovieListPage },
+  { path: 'movies/:imdbId', component: MovieDetailsPage },
+];
